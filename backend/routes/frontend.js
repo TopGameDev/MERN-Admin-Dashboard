1b9b493449bd7ad4fb17badb90d9e74852e3207e
TopGameDev <twintower9875@gmail.com>
2023-10-14 16:06:35 -0500

    Added Transaction Model, Routes, Data

--- backend/routes/frontend.js
+++ backend/routes/frontend.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { getProducts, getCustomers } from "../controllers/frontend.js"
+import { getProducts, getCustomers, getTransactions } from "../controllers/frontend.js"
 
 const router = express.Router()
 
 router.get("/products", getProducts)
 router.get("/customers", getCustomers)
+router.get("/transactions", getTransactions)
 
 export default router;
