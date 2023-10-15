c8e51672b7df65abd926fa6902ca1abc7a95d47c
TopGameDev <twintower9875@gmail.com>
2023-10-15 12:38:27 -0500

    Added Performance Controller,Route,Model

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,8 @@ import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
 import OverallStat from "./models/OverallStat.js";
-import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat } from "./data/index.js";
+import AffiliateStat from "./models/AffilateStat.js";
+import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat, dataAffiliateStat } from "./data/index.js";
 
 // CONFIGURATIONS
 dotenv.config();
@@ -50,5 +51,6 @@ mongoose
     // ProductStat.insertMany(dataProductStat);
     // Transaction.insertMany(dataTransaction);
     // OverallStat.insertMany(dataOverallStat);
+    // AffiliateStat.insertMany(dataAffiliateStat)
   })
   .catch((error) => console.log(`${error} did not connect`));
---
+++ backend/models/AffilateStat.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const AffiliateStatScheme = new mongoose.Schema(
+  {
+    userId: { type: mongoose.Types.ObjectId, ref: "User" },
+    affiliateSales: {
+      type: [mongoose.Types.ObjectId],
+      ref: "Transaction",
+    },
+  },
+  { timestamps: true }
+);
+
+const AffiliateStat = mongoose.model("AffiliateStat", AffiliateStatScheme);
+export default AffiliateStat;
